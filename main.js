3d5d3d9906b00a2625b5d4a4a95530fba25b35e0
Antoserio <antonioloriso822@gmail.com>
2026-03-19 14:18:43 +0100

    cambio voz

--- main.js
+++ main.js
@@ -744,7 +744,7 @@ async function initRealtime() {
                 type: 'session.update',
                 session: {
                     instructions: VIKY_IDENTITY,
-                    voice: 'coral',
+                    voice: 'Shimmer',
                     input_audio_transcription: { model: 'whisper-1' },
                     turn_detection: {
                         type: 'server_vad',
